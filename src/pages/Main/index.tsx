505a125828a5b49493a1294665e8596bf6c85522
Taewoongjung <aipooh8882@naver.com>
2023-03-14 02:43:48 +0900

    render added components

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,56 @@
 import {useObserver} from "mobx-react";
 import {
-    Container, CustomerExtensionBox,
+    Button,
+    Container, Content,
+    CustomerExtensionBox,
     CustomExtension,
-    CustomExtensionInput, CustomExtensionLabel, CustomExtensionSubmit,
+    CustomExtensionInput,
+    CustomExtensionLabel,
+    CustomExtensionSubmit,
     FixedExtension,
-    MainContent,
+    MainContent, RenderComponent,
     Table
 } from "./style";
-
+import {useCallback, useState} from "react";
+import useInput from "../../hooks/useInput";
 
 const Main = () => {
+
+    const onClickAddHandler = useCallback((e:any) => {
+        // axios.post(`http://localhost:8080/seats/${state.thisObject._seatNum}`,
+        //     {customerId, startedTime, endTime}
+        // )
+    }, []);
+
+    const [customerExtension, onChangeCustomerExtension] = useInput<string>('');
+    const [customerExtensionArr, setCustomerExtensionArr] = useState<string[]>([]);
+    const [arrSize, setArrSize] = useState<number>(0);
+
+    const onClick = useCallback((e: any) => {
+        // if (arrSize >= 200) {
+        //     return alert("200개가 한계입니다.");
+        // }
+        setArrSize(customerExtensionArr.length);
+        setCustomerExtensionArr((customerExtensionArr) => {
+            return [...customerExtensionArr, customerExtension];
+        });
+    }, [arrSize, customerExtension]);
+
+    const onClickButton = useCallback((e:any) => {
+        console.log("? = ", e);
+    },[]);
+
+    const renderAddedComponents = () => {
+        const renderedResult: JSX.Element[] = [];
+        for (let i = 0; i < arrSize; i++) {
+            renderedResult.push(
+                <RenderComponent key={i}>
+                    <span>{customerExtensionArr.at(i)}</span>&nbsp;<Button type="button" onClick={() => onClickButton(i)}>✕</Button>
+                </RenderComponent>
+            )
+        }
+        return renderedResult;
+    }
 
     return useObserver(() => (
         <Container>
@@ -20,37 +61,42 @@ const Main = () => {
 
             <MainContent>
                 <Table>
+                    <tbody>
                     <FixedExtension>
                         <td>고정 확장자</td>
                         <td>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
-                            <input type="checkbox" id="ex_chk"/>
+                            <input type="checkbox" id="ex_chk"/> <label>bat</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>cmd</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>com</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>cpl</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>exe</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>scr</label>&nbsp;&nbsp;
+                            <input type="checkbox" id="ex_chk"/> <label>js</label>&nbsp;&nbsp;
                         </td>
                     </FixedExtension>
                     <CustomExtension>
                         <CustomExtensionLabel>커스텀 확장자</CustomExtensionLabel>
                         <td>
-                            <CustomExtensionInput type="text"/>
-                            <CustomExtensionSubmit type="submit" value="+추가"/>
+                            <CustomExtensionInput type="text" onChange={onChangeCustomerExtension}/>
+                            <CustomExtensionSubmit type="submit" onClick={onClick} value="+추가"/>
                         </td>
                     </CustomExtension>
                     <CustomExtension>
                         <td></td>
                         <td>
-                            <CustomerExtensionBox/>
+                            <CustomerExtensionBox>
+                                <>{arrSize}/200</><br/>
+                                <Content>
+                                    {renderAddedComponents()}
+                                </Content>
+                            </CustomerExtensionBox>
                         </td>
                     </CustomExtension>
+                    </tbody>
                 </Table>
             </MainContent>
         </Container>
-    ))
+    ));
 }
 
 export default Main;
